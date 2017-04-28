639d6150c154421b408a12578435085d363a5be9
carlos <carloscarmonaalcantara@gmail.com>
2017-04-28 11:40:27 +0200

    Simplify executor development

--- index.js
+++ index.js
@@ -8,63 +8,48 @@ class checkOnlineService extends Execution {
     super(process);
   }
 
-  exec() {
+  exec(options) {
     var _this = this;
 
-    return new Promise(function (resolve, reject) {
-      _this.getValues()
-        .then((options) => {
-
-          request({
-              method: options.method || 'GET',
-              uri: options.hostname,
-              auth: options.auth
-            },
-            function (err, response, body) {
-              if (err) {
-                var endOptions = {
-                  end: 'error',
-                  messageLog: `Host ${options.hostname} : ${err}`,
-                  execute_err_return: `Host ${options.hostname} ${err}`,
-                  execute_return: ''
-                };
-                _this.end(endOptions, resolve, reject);
-              } else {
-                if (options.check_contains) {
-                  var re = new RegExp(options.check_contains, 'i');
-                  if (re.test(body)) {
-                    var endOptions = {
-                      end: 'end'
-                    };
-                    _this.end(endOptions, resolve, reject);
-                  } else {
-                    var endOptions = {
-                      end: 'error',
-                      messageLog: `Host ${options.hostname} check_contains "${options.check_contains}" test fail.`,
-                      execute_err_return: `Host ${options.hostname} check_contains "${options.check_contains}" test fail.`,
-                      execute_return: ''
-                    };
-                    _this.end(endOptions, resolve, reject);
-                  }
-                } else {
-                  var endOptions = {
-                    end: 'end'
-                  };
-                  _this.end(endOptions, resolve, reject);
-                }
-              }
-            });
-        })
-        .catch((err) => {
+    request({
+        method: options.method || 'GET',
+        uri: options.hostname,
+        auth: options.auth
+      },
+      function (err, response, body) {
+        if (err) {
           var endOptions = {
             end: 'error',
-            messageLog: `checkOnlineService Error getValues: ${err}`,
-            execute_err_return: `checkOnlineService Error getValues ${err}`,
+            messageLog: `Host ${options.hostname} : ${err}`,
+            execute_err_return: `Host ${options.hostname} ${err}`,
             execute_return: ''
           };
-          _this.end(endOptions, resolve, reject);
-        });
-    });
+          _this.end(endOptions);
+        } else {
+          if (options.check_contains) {
+            var re = new RegExp(options.check_contains, 'i');
+            if (re.test(body)) {
+              var endOptions = {
+                end: 'end'
+              };
+              _this.end(endOptions);
+            } else {
+              var endOptions = {
+                end: 'error',
+                messageLog: `Host ${options.hostname} check_contains "${options.check_contains}" test fail.`,
+                execute_err_return: `Host ${options.hostname} check_contains "${options.check_contains}" test fail.`,
+                execute_return: ''
+              };
+              _this.end(endOptions);
+            }
+          } else {
+            var endOptions = {
+              end: 'end'
+            };
+            _this.end(endOptions);
+          }
+        }
+      });
   }
 }
 
